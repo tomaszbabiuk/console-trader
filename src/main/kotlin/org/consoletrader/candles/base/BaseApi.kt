2b32db1495d206105ff9f5d16a54b893947db76c
Łukasz Marczak <lukasz.marczak@intive.com>
2018-01-12 22:00:30 +0100

    base api improvement

--- src/main/kotlin/org/consoletrader/candles/base/BaseApi.kt
+++ src/main/kotlin/org/consoletrader/candles/base/BaseApi.kt
@@ -6,14 +6,17 @@ import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
-abstract class BaseApi<out T>(anApi: Class<T>, endpoint: String) {
+abstract class BaseApi<out T>(anApi: Class<T>,
+                              endpoint: String,
+                              timeout: Long = 30,
+                              timeUnit: TimeUnit = TimeUnit.SECONDS) {
 
     private val api: T
 
     init {
         val client = OkHttpClient.Builder()
-                .connectTimeout(30, TimeUnit.SECONDS)
-                .readTimeout(30, TimeUnit.SECONDS)
+                .connectTimeout(timeout, timeUnit)
+                .readTimeout(timeout, timeUnit)
                 .build()
 
         val builder = Retrofit.Builder()
