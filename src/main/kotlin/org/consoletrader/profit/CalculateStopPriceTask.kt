c1adb26951491d784e30c6b8451f2d01635dac24
agaitomek <tomasz.babiuk@gmail.com>
2018-01-24 21:33:18 +0100

    calculating average market stop price and gain

--- src/main/kotlin/org/consoletrader/profit/CalculateStopPriceTask.kt
+++ src/main/kotlin/org/consoletrader/profit/CalculateStopPriceTask.kt
@@ -15,47 +15,52 @@ class CalculateStopPriceTask(val exchangeManager: ExchangeManager) : Task {
 
     override fun execute(paramsRaw: String) {
         val params = PairExtendedParams(paramsRaw)
-        var totalBuy = 0.0
-        var totalFee = 0.0
-        var totalSell = 0.0
         var currentAmount = 0.0
         var marketPrice = 0.0
+        var iteratedCost = 0.0
+        var amountLeft = 0.0
         Observable
                 .just(accountService.accountInfo.wallet.getBalance(params.currencyPair.base))
-                .doOnNext { currentAmount = it.total.toDouble() }
+                .doOnNext {
+                    currentAmount = it.total.toDouble()
+                    amountLeft = currentAmount
+                }
                 .map { exchangeManager.exchange.marketDataService.getTicker(params.currencyPair) }
-                .doOnNext { marketPrice = it.high.toDouble() }
+                .doOnNext {
+                    marketPrice = it.last.toDouble()
+                }
                 .flatMapIterable {
-                    exchangeManager.getMaximumTradeHistory(params.currencyPair).userTrades
+                    exchangeManager.getMaximumTradeHistory(params.currencyPair).userTrades.reversed()
+                }
+                .filter { it.type == Order.OrderType.BID }
+                .takeUntil {
+                    amountLeft == 0.0
                 }
                 .doOnNext {
-                    val fee = if (it.feeCurrency == params.currencyPair.base) {
-                        it.feeAmount.toDouble() * it.price.toDouble()
-                    } else {
-                        it.feeAmount.toDouble()
-                    }
-
-                    totalFee += fee
-                    if (it.type == Order.OrderType.BID) {
-                        totalBuy += it.originalAmount.toDouble() * it.price.toDouble() - fee
-                    } else {
-                        totalSell += it.originalAmount.toDouble() * it.price.toDouble() - fee
+                    if (amountLeft > 0) {
+                        if (it.originalAmount.toDouble() < amountLeft) {
+                            val feeAmount = if (it.feeCurrency == params.currencyPair.base) {
+                                it.feeAmount.toDouble()
+                            } else {
+                                0.0
+                            }
+                            amountLeft -= it.originalAmount.toDouble() - feeAmount
+                            iteratedCost += it.price.toDouble() * it.originalAmount.toDouble()
+                        } else {
+                            iteratedCost += it.price.toDouble() * amountLeft
+                            amountLeft = 0.0
+                        }
                     }
                 }
                 .doOnComplete {
-                    val buyingCost = totalBuy + totalFee - totalSell
-                    val total = currentAmount * marketPrice - buyingCost
-                    val stopPrice = total/currentAmount
-                    println("Trades buy total: $totalBuy")
-                    println("Trades sell total: $totalSell")
-                    println("Trades fees: $totalFee")
-                    println("Trades cost: $buyingCost")
-                    println("Current amount: $currentAmount${params.currencyPair.base}")
-                    println("Current market price: $marketPrice")
-                    println("==================================")
-                    println("Total: $total")
-                    println("Suggested STOP price: $stopPrice${params.currencyPair.counter}")
+                    val averagePrice = iteratedCost / currentAmount
+                    val roi = 100 - averagePrice/marketPrice * 100
+                    println("Average buying price is: $averagePrice")
+                    println("Current price is: $marketPrice")
+                    println("Return of investment is: $roi%")
                 }
-                .blockingSubscribe()
+                .blockingSubscribe {
+                    println(it)
+                }
     }
 }
