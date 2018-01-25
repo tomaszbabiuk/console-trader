e96d8a1260ae1d14722940892e2da9ca27a2b0a6
agaitomek <tomasz.babiuk@gmail.com>
2018-01-25 20:54:56 +0100

    calculating average market stop price and gain

--- src/main/kotlin/org/consoletrader/profit/CalculateStopPriceTask.kt
+++ src/main/kotlin/org/consoletrader/profit/CalculateStopPriceTask.kt
@@ -2,9 +2,11 @@ package org.consoletrader.profit
 
 import io.reactivex.Observable
 import org.consoletrader.common.ExchangeManager
-import org.consoletrader.common.PairExtendedParams
+import org.consoletrader.common.PairAndDoubleExtendedParams
 import org.consoletrader.common.Task
 import org.knowm.xchange.dto.Order
+
+data class Profit(val averageBuyingPrice: Double, val currentPrice: Double, val returnOfInvestment: Double)
 
 class CalculateStopPriceTask(val exchangeManager: ExchangeManager) : Task {
     private val accountService = exchangeManager.exchange.accountService
@@ -14,7 +16,7 @@ class CalculateStopPriceTask(val exchangeManager: ExchangeManager) : Task {
     }
 
     override fun execute(paramsRaw: String) {
-        val params = PairExtendedParams(paramsRaw)
+        val params = PairAndDoubleExtendedParams(paramsRaw)
         var currentAmount = 0.0
         var marketPrice = 0.0
         var iteratedCost = 0.0
@@ -23,6 +25,10 @@ class CalculateStopPriceTask(val exchangeManager: ExchangeManager) : Task {
                 .just(accountService.accountInfo.wallet.getBalance(params.currencyPair.base))
                 .doOnNext {
                     currentAmount = it.total.toDouble()
+                    if (params.value < currentAmount) {
+                        currentAmount = params.value
+                    }
+
                     amountLeft = currentAmount
                 }
                 .map { exchangeManager.exchange.marketDataService.getTicker(params.currencyPair) }
@@ -37,24 +43,28 @@ class CalculateStopPriceTask(val exchangeManager: ExchangeManager) : Task {
                     amountLeft == 0.0
                 }
                 .doOnNext {
-                    if (amountLeft > 0) {
-                        if (it.originalAmount.toDouble() < amountLeft) {
-                            val feeAmount = if (it.feeCurrency == params.currencyPair.base) {
-                                it.feeAmount.toDouble()
+                    val feeCurrency = if (it.feeCurrency == params.currencyPair.base) {
+                        it.feeAmount.toDouble()
+                    } else {
+                        0.0
+                    }
+
+                    if (it.type == Order.OrderType.BID) {
+                        if (amountLeft > 0) {
+                            if (it.originalAmount.toDouble() < amountLeft) {
+
+                                amountLeft -= it.originalAmount.toDouble() - feeCurrency
+                                iteratedCost += it.price.toDouble() * it.originalAmount.toDouble()
                             } else {
-                                0.0
+                                iteratedCost += it.price.toDouble() * amountLeft
+                                amountLeft = 0.0
                             }
-                            amountLeft -= it.originalAmount.toDouble() - feeAmount
-                            iteratedCost += it.price.toDouble() * it.originalAmount.toDouble()
-                        } else {
-                            iteratedCost += it.price.toDouble() * amountLeft
-                            amountLeft = 0.0
                         }
                     }
                 }
                 .doOnComplete {
                     val averagePrice = iteratedCost / currentAmount
-                    val roi = 100 - averagePrice/marketPrice * 100
+                    val roi = 100 - averagePrice / marketPrice * 100
                     println("Average buying price is: $averagePrice")
                     println("Current price is: $marketPrice")
                     println("Return of investment is: $roi%")
